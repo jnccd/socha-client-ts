8ef43f665cb8d1ee8d31c27305edc1a49903e2cf
Niklas Carstensen <niklas.carstensen@outlook.de>
2023-02-04 02:16:28 +0100

    Remove untracked files

--- output/logic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.decideMove = void 0;
-const main_js_1 = require("./main.js");
-function decideMove(board, turn, currentPlayer) {
-    let moves = (0, main_js_1.getPossibleMoves)(turn, board);
-    return moves[Math.floor(Math.random() * moves.length)];
-}
-exports.decideMove = decideMove;
